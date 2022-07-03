67adf8b5b65b455e10e14418762a424184b049b4
MarcosvBueno <mvbueno07@gmail.com>
2022-07-03 14:03:27 -0300

    adiciona atividade do curso javascript

---
+++ Curso Javascript/orientação-a-objetos/script.js
@@ -0,0 +1,69 @@
+class contaBancaraia {
+  constructor(agencia, numero, tipo, saldo) {
+    this.agencia = agencia
+    this.numero = numero
+    this.tipo = tipo
+    this._saldo = 0
+  }
+
+  get saldo() {
+    return this._saldo
+  }
+
+  set saldo(valor) {
+    this._saldo = valor
+  }
+
+  sacar(valor) {
+    if (valor > this._saldo) {
+      return 'Operação Negada'
+    }
+    this.saldo = this._saldo - valor
+
+    return this._saldo
+  }
+
+  depositar(valor) {
+    this._saldo = this._saldo + valor
+
+    return this._saldo
+  }
+}
+
+class contaCorrente extends contaBancaraia {
+  constructor(agencia, numero, cartaoCredito) {
+    super(agencia, numero)
+    this.tipo = 'corrente'
+    this._cartaoCredito = cartaoCredito
+  }
+
+  get cartaoCredito() {
+    return this._cartaoCredito
+  }
+
+  set cartaoCredito(valor) {
+    this._cartaoCredito = valor
+  }
+}
+
+class ContaPoupanca extends contaBancaraia {
+  constructor(agencia, numero) {
+    super(agencia, numero)
+    this.tipo = 'poupança'
+  }
+}
+
+class ContaUnicersitaria extends contaBancaraia {
+  constructor(agencia, numero) {
+    super(agencia, numero)
+    this.tipo = 'universitária'
+  }
+
+  sacar(valor) {
+    if (valor > 500) {
+      return 'Operação Negada'
+    }
+
+    this._saldo = this._saldo - valor
+  }
+}
