a86c2c9731be2a8a235e7444760874f58f7c790f
MarcosvBueno <mvbueno07@gmail.com>
2022-07-03 23:15:10 -0300

    adiciona atividade de modulos do curso javascript

---
+++ Curso Javascript/modulos/atividade/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from './funcoes.mjs'
+
+console.log(mostraIdade('Marcos', '18'))
+console.log(mostraCidade('Marcos', 'Olinda'))
+console.log(mostraHobby('Marcos', 'jogar vôlei'))
